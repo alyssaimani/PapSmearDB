2926f063191c595912d581ae8d978b9f00bbf606
alyssaimani <alyssaimani44@gmail.com>
2025-08-04 11:06:59 +0700

    remove routing static

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -26,6 +26,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url=reverse_lazy('admin:index'), permanent=True))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
